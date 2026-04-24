da0d1550293d9156877a8ac3a69357e0e589c1b2
legendcodes1 <olafaruq1@gmail.com>
2026-04-23 21:04:53 -0500

    added recommendations

--- ReliefHub/src/routes/exerciseRoute.ts
+++ ReliefHub/src/routes/exerciseRoute.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getExerciseController, createExerciseController, deleteExerciseController, updateExerciseController } from "../controllers/excersiseController.js";
+import { getExerciseController, createExerciseController, deleteExerciseController, updateExerciseController, getRecommendedExercises } from "../controllers/excersiseController.js";
 
 const exerciseRouter = Router();
+exerciseRouter.get("/recommendations", getRecommendedExercises);
 
 exerciseRouter.get("/", getExerciseController)
 exerciseRouter.post("/", createExerciseController)
 exerciseRouter.put("/:id", updateExerciseController)
 exerciseRouter.delete("/:id", deleteExerciseController)
+
 
 export default exerciseRouter
--- ReliefHub/src/service/routineCompletionsService.ts
+++ ReliefHub/src/service/routineCompletionsService.ts
@@ -1,10 +1,12 @@
+import discomfortType from "../dto/discomfortType.js"
+import { routineType } from "../dto/routineType.js"
 import { prisma } from "../lib/prisma.js"
 
 export const getRoutineCompleteRepo = async () =>{
      return prisma.routine_completions.findMany()
 }
 
-export const createDiscomfortTypeRepo = async (data: routineType) =>{
+export const createDiscomfortTypeRepo = async (data: discomfortType) =>{
      return prisma.discomfort_types.create({
        data: {
             ...data,
