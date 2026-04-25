ecc06fedefff8f3457c742133285e776ea82716a
legendcodes1 <olafaruq1@gmail.com>
2026-04-25 11:18:34 -0500

    refactored to not using repo and just service

--- ReliefHub/src/service/routineService.ts
+++ ReliefHub/src/service/routineService.ts
@@ -1,18 +1,29 @@
-import { routineType } from "../dto/routineType.js"
-import { getRoutineCompletionsRepo, createRoutineCompletionsRepo, updateRoutineCompletionsRepo, deleteSavedExerciseRepo  } from "../repositories/routineRepo.js"
+import { routineType } from "../dto/routineType.js";
+import { prisma } from "../lib/prisma.js";
+export const getRoutineService = async (userId: string) => {
+  return prisma.routine_completions.findMany({
+    where: { user_id: userId },
+    include: {
+      exercises: true, // THIS MEANS TO JOIN EXERCISE DATA
+    },
+  });
+};
 
-export const getRoutineService = async(userId: string) =>{
-    return await getRoutineCompletionsRepo(userId)
-}
+export const createRoutineService = async (data: routineType) => {
+  return prisma.routine_completions.create({
+    data,
+  });
+};
 
-export const createRoutineService = async(data: routineType) =>{
-    return createRoutineCompletionsRepo(data)
-}
+export const updateRoutineService = async (id: string, data: routineType) => {
+  return prisma.routine_completions.update({
+    where: { id },
+    data: { ...data },
+  });
+};
 
-export const updateRoutineService = async(id: string, data: routineType) =>{
-    return updateRoutineCompletionsRepo(id, data)
-}
-
-export const deleteRoutineService = async(id: string) =>{
-    return deleteSavedExerciseRepo(id)
-}
+export const deleteRoutineService = async (id: string) => {
+  return prisma.routine_completions.delete({
+    where: { id: id },
+  });
+};
